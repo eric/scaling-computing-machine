b15778147ef0aa5ce75544e074f085577cbcb905
Eric Lindvall <eric@5stops.com>
2013-09-20 00:52:32 -0700

    updates

--- script.sh
+++ script.sh
@@ -10,5 +10,9 @@ traceroute -A s3-2.amazonaws.com
 
 curl -o /dev/null http://s3.amazonaws.com/highspot_artifacts/zipfiles/solr-4.2.1.zip
 
+curl -o /dev/null http://s3-1.amazonaws.com/highspot_artifacts/zipfiles/solr-4.2.1.zip
+
+curl -o /dev/null http://s3-2.amazonaws.com/highspot_artifacts/zipfiles/solr-4.2.1.zip
+
 # sudo sbin/pchar -s 2 -v -d 1 s3.amazonaws.com
 
