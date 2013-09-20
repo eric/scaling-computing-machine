a49d2027c7d9e9cec4462d8d5cafdeb1df8f77e3
Eric Lindvall <eric@5stops.com>
2013-09-19 23:07:31 -0700

    try pathchar

--- script.sh
+++ script.sh
@@ -6,6 +6,6 @@ traceroute s3.amazonaws.com
 
 curl -o /dev/null http://s3.amazonaws.com/highspot_artifacts/zipfiles/solr-4.2.1.zip
 
-pchar s3.amazonaws.com
+pathchar s3.amazonaws.com
 
 scriptroute --help
