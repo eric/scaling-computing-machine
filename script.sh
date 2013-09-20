b68fbe4f4ce92ddb520f5c737fe8222eca23b53b
Eric Lindvall <eric@5stops.com>
2013-09-20 00:45:18 -0700

    updates

--- script.sh
+++ script.sh
@@ -4,6 +4,10 @@ set -x
 
 traceroute -A s3.amazonaws.com
 
+traceroute -A s3-1.amazonaws.com
+
+traceroute -A s3-2.amazonaws.com
+
 ping -c 20 -i 0.2 s3.amazonaws.com
 
 curl -o /dev/null http://s3.amazonaws.com/highspot_artifacts/zipfiles/solr-4.2.1.zip
